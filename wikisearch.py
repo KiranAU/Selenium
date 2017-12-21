37f661fe80489ae50f139f6491192b7242129449
Kiran Uttarkar <kiranuttarkar5@gmail.com>
2017-12-21 12:35:47 -0500

    Create wikisearch.py

---
+++ wikisearch.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("https://www.wikipedia.org/")
+        self.assertIn("Wiki", driver.title)
+        elem = driver.find_element_by_id("js-link-box-en")
+        elem.send_keys(Keys.RETURN)
+        elem = driver.find_element_by_id("searchInput")
+        elem.send_keys("USA")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
